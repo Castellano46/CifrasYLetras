6020598ae932bbec2efc2a87dcae2a761ad82b1e
Castellano46 <pedroliebana35@gmail.com>
2024-06-17 17:37:03 +0200

    primer commit

---
+++ CifrasYLetras/CifrasYLetrasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CifrasYLetrasApp.swift
+//  CifrasYLetras
+//
+//  Created by Pedro on 17/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CifrasYLetrasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
