e5bcb5746b30c6878968e7a5b06c615d660fe23e
Castellano46 <pedroliebana35@gmail.com>
2024-07-29 09:41:51 +0200

    HowToPlayReview

--- CifrasYLetras/Views/HowToPlayView.swift
+++ CifrasYLetras/Views/HowToPlayView.swift
@@ -22,21 +22,21 @@ struct HowToPlayView: View {
                     VStack(alignment: .leading, spacing: 20) {
                         SectionHeaderView(title: "Juego de Letras")
                         
-                        Text("En el Juego de Letras, debes formar la palabra más larga posible usando las letras seleccionadas. Selecciona vocales y consonantes para completar las 10 letras. Por cada letra utilizada en tu palabra obtendrás un punto. El temporizador comenzará una vez que se hayan seleccionado todas las letras.")
+                        Text("En el Juego de Letras, debes formar la palabra más larga posible usando las letras seleccionadas. Pulsa sobre el botón ***V*** si quieres que el siguiente cuadrado se rellene con una vocal o el botón ***C*** si por el contrario prefieres una consonante hasta completar las 10 letras. Por cada letra utilizada en tu palabra obtendrás un punto que se sumará a tu contador final ⭐️. El temporizador comenzará una vez que se hayan seleccionado todas las letras.")
                             .font(.custom("Marker Felt", size: 28))
                             .fixedSize(horizontal: false, vertical: true)
                             .padding(.bottom, 20)
                         
                         SectionHeaderView(title: "Juego de Cifras")
                         
-                        Text("En el Juego de Cifras, debes llegar al número objetivo utilizando los números seleccionados y las operaciones matemáticas elementales. Pulsa sobre el botón ***Número*** para completar los 6 números con los que jugarás. Cuando todos tus números estén en su casilla correspondiente aparecerá el número objetivo. Si al utilizar las operaciones llegas a una cifra aproximada obtendrás 5 puntos y si llegas a la exacta obtendrás 10 puntos. El temporizador comenzará una vez que se hayan seleccionado todos los números.")
+                        Text("En el Juego de Cifras, debes llegar al número objetivo utilizando los números seleccionados y las operaciones matemáticas elementales. Pulsa sobre el botón 🔢 para completar los 6 números con los que jugarás. Cuando todos tus números estén en su casilla correspondiente aparecerá el número objetivo, al cual deberás llegar o aproximarte utilizando las operaciones básicas. Si al utilizar las operaciones llegas a una cifra aproximada obtendrás 5 puntos y si llegas a la exacta obtendrás 10 puntos. Podrás borrar las operaciones que has utilizado pulsando sobre el boton ↩️. El temporizador comenzará una vez que se hayan seleccionado todos los números.")
                             .font(.custom("Marker Felt", size: 28))
                             .fixedSize(horizontal: false, vertical: true)
                             .padding(.bottom, 20)
                         
                         SectionHeaderView(title: "Temporizador")
                         
-                        Text("El temporizador se inicia automáticamente una vez que todas las letras o números hayan sido seleccionados. Puedes pausar y reanudar el temporizador según sea necesario. Si el temporizador se encuentra en ***Pausa*** no podrás realizar ninguna acción en el juego.")
+                        Text("El temporizador se inicia automáticamente una vez que todas las letras o números hayan sido seleccionados. Cuando el juego comienze contarás con 40 segundos para encontrar la palabra más larga, en el juego de ***'Letras'*** o el número objetivo en el juego de ***'Cifras'***. Puedes pausar y reanudar el temporizador según sea necesario. Si el temporizador se encuentra en ***Pausa*** no podrás realizar ninguna acción en el juego.")
                             .font(.custom("Marker Felt", size: 28))
                             .fixedSize(horizontal: false, vertical: true)
                     }
